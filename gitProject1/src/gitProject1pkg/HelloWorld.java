71d0c0af60062786cc150473161b06d405492fd7
venkatesh b <venkib919@gmail.com>
2017-10-15 21:39:54 +0530

    First Commit

---
+++ gitProject1/src/gitProject1pkg/HelloWorld.java
@@ -0,0 +1,11 @@
+package gitProject1pkg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	System.out.println("First Programme");
+
+	}
+
+}
